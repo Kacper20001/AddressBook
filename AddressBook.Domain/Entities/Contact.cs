eb87f37494542df385392827154560fbff7de751
Kacper Kulig <kuligkacper20001@gmail.com>
2025-06-12 18:19:42 +0200

    feat(domain): add full EF Core annotations to Contact & Location -Mapped tables explicitly -Added validation & foreign key attributes -Ensures accurate schema in migrations

---
+++ AddressBook.Domain/Entities/Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Domain.Entities
+{
+    /// <summary>
+    /// Represents a contact in the address book.
+    /// </summary>
+    [Table("Contacts")]
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        [ForeignKey("Location")]
+        public int LocationId { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
---
+++ AddressBook.Domain/Entities/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Domain.Entities
+{
+    /// <summary>
+    /// Represents a postal location with code and name.
+    /// </summary>
+    [Table("Locations")]
+    public class Location
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string CityName { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
+    }
+}
