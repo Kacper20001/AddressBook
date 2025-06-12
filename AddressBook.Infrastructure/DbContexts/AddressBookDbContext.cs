4e03fdcdf00b44166d3e09a10241ba6e701abc9a
Kacper Kulig <kuligkacper20001@gmail.com>
2025-06-12 18:43:09 +0200

    added System.Configuration.ConfigurationManager to support connection string reading – Installed NuGet package for accessing App.config connectionStrings – Unblocked EF Core database configuration in Program.cs – Created AddressBookDbContext class to handle EF Core database context

---
+++ AddressBook.Infrastructure/DbContexts/AddressBookDbContext.cs
@@ -0,0 +1,35 @@
+﻿using AddressBook.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Infrastructure.DbContexts
+{
+    /// <summary>
+    /// Main EF Core DbContext for the address book.
+    /// </summary>
+    public class AddressBookDbContext : DbContext
+    {
+        public AddressBookDbContext(DbContextOptions<AddressBookDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>()
+                .HasOne(c => c.Location)
+                .WithMany(l => l.Contacts)
+                .HasForeignKey(c => c.LocationId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- AddressBook.UI.WinForms/Program.cs
+++ AddressBook.UI.WinForms/Program.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AddressBook.Infrastructure.DbContexts;
 
 namespace AddressBook.UI.WinForms
 {
     static class Program
     {
         /// <summary>
-        /// Główny punkt wejścia dla aplikacji.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<AddressBookDbContext>(options =>
+                options.UseSqlServer(
+                    System.Configuration.ConfigurationManager
+                        .ConnectionStrings["AddressBookDb"]
+                        .ConnectionString));
+
+            var serviceProvider = services.BuildServiceProvider();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
